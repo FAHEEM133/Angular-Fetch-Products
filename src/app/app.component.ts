35831b3bfcc9012a31d0218d305342baf474cbc5
FAHEEM133 <mhd.faheem00@gmail.com>
2024-07-25 23:35:40 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CardsComponent } from "./cards/cards.component";
+import { Product } from './interface/product';
+import { CommonModule } from '@angular/common';
+ 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardsComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'angular-fetch-api-task';
+  categorizedProducts: { [key: string]: Product[]} = {};
+ 
+  async ngOnInit() {
+    try {
+      const response = await fetch("https://dummyjson.com/products");
+      const data = await response.json();
+      this.categorizeProducts(data.products);
+      console.log(this.categorizedProducts);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+ 
+  categorizeProducts(products: Product[]) {
+    this.categorizedProducts = products.reduce((acc, product) => {
+      if (!acc[product.category]) {
+        acc[product.category] = [];
+      }
+      acc[product.category].push(product);
+      return acc;
+    }, {} as { [key: string]: Product[] });
+  }
+  getCategories(): string[] {
+    return Object.keys(this.categorizedProducts);
+  }
+ 
+  getProductsByCategory(category: string): Product[] {
+    return this.categorizedProducts[category];
+  }
 }
